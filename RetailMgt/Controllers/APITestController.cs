a35e04eea14384e592c7329a6c428cc0b9c169fc
rishabh2496 <rishabhsharma24111996@gmail.com>
2020-05-04 03:37:14 +1200

    Add project files.

---
+++ RetailMgt/Controllers/APITestController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RetailMgt.Controllers
+{
+    public class APITestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ RetailMgt/Controllers/Suppliers_APIController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RetailMgt.Data;
+using RetailMgt.Models;
+
+namespace RetailMgt.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Suppliers_API")]
+    public class Suppliers_APIController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public Suppliers_APIController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Suppliers_API
+        [HttpGet]
+        public IEnumerable<Supplier> GetSupplier()
+        {
+            return _context.Supplier;
+        }
+
+        // GET: api/Suppliers_API/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetSupplier([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var supplier = await _context.Supplier.SingleOrDefaultAsync(m => m.ID == id);
+
+            if (supplier == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(supplier);
+        }
+
+        // PUT: api/Suppliers_API/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSupplier([FromRoute] int id, [FromBody] Supplier supplier)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != supplier.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(supplier).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SupplierExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Suppliers_API
+        [HttpPost]
+        public async Task<IActionResult> PostSupplier([FromBody] Supplier supplier)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Supplier.Add(supplier);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetSupplier", new { id = supplier.ID }, supplier);
+        }
+
+        // DELETE: api/Suppliers_API/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSupplier([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var supplier = await _context.Supplier.SingleOrDefaultAsync(m => m.ID == id);
+            if (supplier == null)
+            {
+                return NotFound();
+            }
+
+            _context.Supplier.Remove(supplier);
+            await _context.SaveChangesAsync();
+
+            return Ok(supplier);
+        }
+
+        private bool SupplierExists(int id)
+        {
+            return _context.Supplier.Any(e => e.ID == id);
+        }
+    }
+}
